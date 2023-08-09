b41555e7315683fda75e1a67d147705f8df311a6
Nick Vantine <nickav200432@gmail.com>
2023-08-09 15:08:04 -0500

    Finished Tutorial

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -23,6 +23,22 @@ function App() {
     setFilters(searchParams);
   };
 
+  const deleteItem = (item) => {
+    const items = data["items"];
+    const requestOptions = {
+      method: "DELETE",
+    };
+    fetch(`http://localhost:3000/items/${item.id}`, requestOptions).then(
+      (response) => {
+        if (response.ok) {
+          const idx = items.indexOf(item);
+          items.splice(idx, 1);
+          setData({ items });
+        }
+      }
+    );
+  };
+
   const addItemToData = (item) => {
     let items = data["items"];
 
@@ -73,7 +89,10 @@ function App() {
           <AddItem addItem={addItemToData} />
         </div>
         <div className="col-9">
-          <ItemsDisplay items={filterData(data["items"])} />
+          <ItemsDisplay
+            deleteItem={deleteItem}
+            items={filterData(data["items"])}
+          />
         </div>
       </div>
     </div>
