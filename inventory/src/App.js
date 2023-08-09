747df79001deebddacdd14c7cf7050b2e38199ad
Nick Vantine <nickav200432@gmail.com>
2023-08-08 19:47:56 -0500

    #9 - Filtering/Searching for Data

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -4,18 +4,46 @@ import { useState } from "react";
 import ItemsDisplay from "./ItemsDisplay";
 
 function App() {
-  const [filters, setFilters] = useState({});
+  const [filters, setFilters] = useState({
+    name: "",
+    price: 0,
+    type: "",
+    brand: "",
+  });
+
   const [data, setData] = useState({ items: [] });
 
   const updateFilters = (searchParams) => {
     setFilters(searchParams);
   };
+
   const addItemToData = (item) => {
     let items = data["items"];
     item.id = items.length;
     items.push(item);
     setData({ items });
     console.log(data);
+  };
+
+  const filterData = (items) => {
+    const filteredData = [];
+
+    for (const item of items) {
+      if (filters.name !== "" && item.name !== filters.name) {
+        continue;
+      }
+      if (filters.price !== 0 && item.price > filters.price) {
+        continue;
+      }
+      if (filters.type !== "" && item.type !== filters.type) {
+        continue;
+      }
+      if (filters.brand !== "" && item.brand !== filters.brand) {
+        continue;
+      }
+      filteredData.push(item);
+    }
+    return filteredData;
   };
 
   return (
@@ -27,7 +55,7 @@ function App() {
         <AddItem addItem={addItemToData} />
       </div>
       <div className="row mt-3">
-        <ItemsDisplay items={data["items"]} />
+        <ItemsDisplay items={filterData(data["items"])} />
       </div>
     </div>
   );
